345aca511dc6476ba406d599a83f9f9fbfff2672
Abhishek Konar <48175493+abhishek-konar@users.noreply.github.com>
2019-10-18 19:30:03 +0530

    Add files via upload

---
+++ build/precache-manifest.a9bed885a551af2bc089701d432d6264.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bf42080aedd19e9b4e51756ef132fa9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c528906cce68365c6472",
+    "url": "/static/css/main.7af7a3cd.chunk.css"
+  },
+  {
+    "revision": "a174277d71706e06af75",
+    "url": "/static/js/2.b3a1b1e9.chunk.js"
+  },
+  {
+    "revision": "c528906cce68365c6472",
+    "url": "/static/js/main.e91e895e.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  },
+  {
+    "revision": "773ad8f1dc64e202d6b9f892af446d60",
+    "url": "/static/media/background1.773ad8f1.jpg"
+  }
+]);
