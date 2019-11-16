661816174f62a2d777b5e5483e4daafcdc8f7ce4
Abhishek Konar <48175493+abhishek-konar@users.noreply.github.com>
2019-11-16 17:51:46 +0530

    4

---
+++ build/precache-manifest.7eb9937722bd89212984fcfcafab9001.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "967ef5fcb24a03914df161e2a424cc1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9df8885534c777a917b",
+    "url": "/static/css/main.43423fa0.chunk.css"
+  },
+  {
+    "revision": "d8bb4b3aa38567ae105f",
+    "url": "/static/js/2.aa01e62c.chunk.js"
+  },
+  {
+    "revision": "b9df8885534c777a917b",
+    "url": "/static/js/main.5c9d980a.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  },
+  {
+    "revision": "1ad5f9a0df0e96de3d5c758cc6b7a617",
+    "url": "/static/media/background1.1ad5f9a0.jpg"
+  }
+]);
